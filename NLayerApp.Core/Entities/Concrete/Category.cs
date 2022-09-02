7afae3280bfb3fc6926a8f5d080c1ddf896c176c
AloKzMstf <alokazamustafa@gmail.com>
2022-09-02 15:07:45 +0300

    Core(CL) => Entities(Dir) => Abstract, Concrete klasörleri eklendi. Abstract(Dir) =>BaseEntity, Concrete(Dir) =>Category, Product, ProductFeature sınıfları eklendi. BaseEntity, Category, Product sınıflarına kalıtım verildi.

---
+++ NLayerApp.Core/Entities/Concrete/Category.cs
@@ -0,0 +1,12 @@
+﻿using NLayerApp.Core.Entities.Abstract;
+
+namespace NLayerApp.Core.Entities.Concrete
+{
+    public class Category : BaseEntity
+    {
+        public string? Name { get; set; }
+
+        //Navigation property
+        public ICollection<Product>? Products { get; set; }
+    }
+}
---
+++ NLayerApp.Core/Entities/Concrete/Product.cs
@@ -0,0 +1,17 @@
+﻿using NLayerApp.Core.Entities.Abstract;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace NLayerApp.Core.Entities.Concrete
+{
+    public class Product : BaseEntity
+    {
+        public string? Name { get; set; }
+        public int Stock { get; set; }
+        public decimal Price { get; set; }
+
+        //[ForeignKey("Category_Id")] = eğer farklı isim verirsek bu attribute'ü kullanırız. 
+        public int CategoryId { get; set; }
+        public Category? Category { get; set; }
+        public ProductFeature? ProductFeature { get; set; }
+    }
+}

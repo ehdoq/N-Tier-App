dac04a741949d93e1d548503c69acce0afe43b97
AloKzMstf <alokazamustafa@gmail.com>
2022-09-02 15:39:44 +0300

    Repository(CL) => Seeds(Dir) eklendi ve Category, Product, ProductFeature Seed'leri eklendi. AppDbContext(Class) sınıfına dahil edildi.

--- NLayerApp.Repository/AppDBContext/AppDbContext.cs
+++ NLayerApp.Repository/AppDBContext/AppDbContext.cs
@@ -21,6 +21,19 @@ namespace NLayerApp.Repository.AppDBContext
 
             //modelBuilder.ApplyConfiguration(new ProductConfiguration());
 
+            modelBuilder.Entity<ProductFeature>().HasData
+            (
+                new ProductFeature() { Id = 1, Color = "Kırmızı", Height = 10, Width = 10, ProductId = 1 },
+                new ProductFeature() { Id = 2, Color = "Mavi", Height = 10, Width = 10, ProductId = 2 },
+                new ProductFeature() { Id = 3, Color = "Sarı", Height = 10, Width = 10, ProductId = 3 },
+                new ProductFeature() { Id = 4, Color = "Kırmızı", Height = 30, Width = 20, ProductId = 4 },
+                new ProductFeature() { Id = 5, Color = "Mavi", Height = 30, Width = 20, ProductId = 5 },
+                new ProductFeature() { Id = 6, Color = "Sarı", Height = 30, Width = 20, ProductId = 6 },
+                new ProductFeature() { Id = 7, Color = "Kırmızı", Height = 50, Width = 30, ProductId = 7 },
+                new ProductFeature() { Id = 8, Color = "Mavi", Height = 50, Width = 30, ProductId = 8 },
+                new ProductFeature() { Id = 9, Color = "Sarı", Height = 50, Width = 30, ProductId = 9 }
+            );
+
             base.OnModelCreating(modelBuilder);
         }
     }
---
+++ NLayerApp.Repository/Seeds/ProductSeed.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using NLayerApp.Core.Entities.Concrete;
+
+namespace NLayerApp.Repository.Seeds
+{
+    public class ProductSeed : IEntityTypeConfiguration<Product>
+    {
+        public void Configure(EntityTypeBuilder<Product> builder)
+        {
+            builder.HasData
+            (
+                new Product() { Id = 1, CategoryId = 1, Name = "Kurşun kalem", Price = 100, Stock = 10, CreatedDate = DateTime.Now },
+                new Product() { Id = 2, CategoryId = 1, Name = "Dolma kalem", Price = 200, Stock = 20, CreatedDate = DateTime.Now },
+                new Product() { Id = 3, CategoryId = 1, Name = "Uçlu kalem", Price = 300, Stock = 30, CreatedDate = DateTime.Now },
+                new Product() { Id = 4, CategoryId = 2, Name = "Roman", Price = 400, Stock = 40, CreatedDate = DateTime.Now },
+                new Product() { Id = 5, CategoryId = 2, Name = "Bilim kurgu", Price = 500, Stock = 50, CreatedDate = DateTime.Now },
+                new Product() { Id = 6, CategoryId = 2, Name = "Gerilim", Price = 600, Stock = 60, CreatedDate = DateTime.Now },
+                new Product() { Id = 7, CategoryId = 3, Name = "Çizgili defter", Price = 700, Stock = 70, CreatedDate = DateTime.Now },
+                new Product() { Id = 8, CategoryId = 3, Name = "Kareli defter", Price = 800, Stock = 80, CreatedDate = DateTime.Now },
+                new Product() { Id = 9, CategoryId = 3, Name = "Çizgisiz defter", Price = 900, Stock = 90, CreatedDate = DateTime.Now }
+            );
+        }
+    }
+}

a8b4398765c708c1c220d075d6570b64a3230476
AloKzMstf <alokazamustafa@gmail.com>
2022-09-02 16:01:54 +0300

    Repository(CL) => Repositories(Dir), UnitOfWorks(Dir) eklendi ve sırasıyla Repository(Class), UnitOfWork(Class) sınıfları eklendi.

---
+++ NLayerApp.Repository/UnitOfWorks/UnitOfWork.cs
@@ -0,0 +1,24 @@
+﻿using NLayerApp.Core.UnitOfWorks;
+using NLayerApp.Repository.AppDBContext;
+
+namespace NLayerApp.Repository.UnitOfWorks
+{
+    public class UnitOfWork : IUnitOfWork
+    {
+        private readonly AppDbContext _appDbContext;
+        public UnitOfWork(AppDbContext appDbContext)
+        {
+            _appDbContext = appDbContext;
+        }
+
+        public void Commit()
+        {
+            _appDbContext.SaveChanges();
+        }
+
+        public async Task? CommitAsync()
+        {
+            await _appDbContext.SaveChangesAsync();
+        }
+    }
+}

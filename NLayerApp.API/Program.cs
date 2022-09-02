b0f3dde8f74597fdab29c80aa9ac05e29191530d
AloKzMstf <alokazamustafa@gmail.com>
2022-09-02 16:18:56 +0300

    API(API) => appsetting.json dosyasına ConnectionString eklendi. Program.cs dosyasına DbContext ve DI eklendi. Microsoft.EntityFrameworkCore.Design paketi yüklendi.

--- NLayerApp.API/Program.cs
+++ NLayerApp.API/Program.cs
@@ -1,3 +1,13 @@
+using Microsoft.EntityFrameworkCore;
+using NLayerApp.Core.Repositories;
+using NLayerApp.Core.Services;
+using NLayerApp.Core.UnitOfWorks;
+using NLayerApp.Repository.AppDBContext;
+using NLayerApp.Repository.Repositories;
+using NLayerApp.Repository.UnitOfWorks;
+using NLayerApp.Service.Services;
+using System.Reflection;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +16,22 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+#region DbContext
+builder.Services.AddDbContext<AppDbContext>(x =>
+{
+    x.UseSqlServer(builder.Configuration.GetConnectionString("sqlCon"), options =>
+    {
+        options.MigrationsAssembly(Assembly.GetAssembly(typeof(AppDbContext))?.GetName().Name);
+    });
+});
+#endregion
+
+#region DI
+builder.Services.AddScoped(typeof(IRepository<>), typeof(Repository<>));
+builder.Services.AddScoped(typeof(IService<>), typeof(Service<>));
+builder.Services.AddScoped<IUnitOfWork, UnitOfWork>();
+#endregion
 
 var app = builder.Build();
 

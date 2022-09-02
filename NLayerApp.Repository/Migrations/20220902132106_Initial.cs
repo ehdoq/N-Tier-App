9e92b0591e0defdec052c5cab59265e540e1208a
AloKzMstf <alokazamustafa@gmail.com>
2022-09-02 16:21:56 +0300

    Repository(CL) => Package Manager Console => Add-Migration Initial

---
+++ NLayerApp.Repository/Migrations/20220902132106_Initial.cs
@@ -0,0 +1,144 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace NLayerApp.Repository.Migrations
+{
+    public partial class Initial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Categories",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: false),
+                    CreatedDate = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    UpdateDate = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Categories", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Products",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: false),
+                    Stock = table.Column<int>(type: "int", nullable: false),
+                    Price = table.Column<decimal>(type: "decimal(18,2)", nullable: false),
+                    CategoryId = table.Column<int>(type: "int", nullable: false),
+                    CreatedDate = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    UpdateDate = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Products", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Products_Categories_CategoryId",
+                        column: x => x.CategoryId,
+                        principalTable: "Categories",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "ProductFeatures",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Color = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: false),
+                    Height = table.Column<int>(type: "int", nullable: false),
+                    Width = table.Column<int>(type: "int", nullable: false),
+                    ProductId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ProductFeatures", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_ProductFeatures_Products_ProductId",
+                        column: x => x.ProductId,
+                        principalTable: "Products",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.InsertData(
+                table: "Categories",
+                columns: new[] { "Id", "CreatedDate", "Name", "UpdateDate" },
+                values: new object[] { 1, new DateTime(2022, 9, 2, 16, 21, 5, 778, DateTimeKind.Local).AddTicks(6051), "Kalemler", new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified) });
+
+            migrationBuilder.InsertData(
+                table: "Categories",
+                columns: new[] { "Id", "CreatedDate", "Name", "UpdateDate" },
+                values: new object[] { 2, new DateTime(2022, 9, 2, 16, 21, 5, 778, DateTimeKind.Local).AddTicks(6063), "Kitaplar", new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified) });
+
+            migrationBuilder.InsertData(
+                table: "Categories",
+                columns: new[] { "Id", "CreatedDate", "Name", "UpdateDate" },
+                values: new object[] { 3, new DateTime(2022, 9, 2, 16, 21, 5, 778, DateTimeKind.Local).AddTicks(6065), "Defterler", new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified) });
+
+            migrationBuilder.InsertData(
+                table: "Products",
+                columns: new[] { "Id", "CategoryId", "CreatedDate", "Name", "Price", "Stock", "UpdateDate" },
+                values: new object[,]
+                {
+                    { 1, 1, new DateTime(2022, 9, 2, 16, 21, 5, 778, DateTimeKind.Local).AddTicks(6278), "Kurşun kalem", 100m, 10, new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified) },
+                    { 2, 1, new DateTime(2022, 9, 2, 16, 21, 5, 778, DateTimeKind.Local).AddTicks(6281), "Dolma kalem", 200m, 20, new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified) },
+                    { 3, 1, new DateTime(2022, 9, 2, 16, 21, 5, 778, DateTimeKind.Local).AddTicks(6282), "Uçlu kalem", 300m, 30, new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified) },
+                    { 4, 2, new DateTime(2022, 9, 2, 16, 21, 5, 778, DateTimeKind.Local).AddTicks(6283), "Roman", 400m, 40, new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified) },
+                    { 5, 2, new DateTime(2022, 9, 2, 16, 21, 5, 778, DateTimeKind.Local).AddTicks(6284), "Bilim kurgu", 500m, 50, new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified) },
+                    { 6, 2, new DateTime(2022, 9, 2, 16, 21, 5, 778, DateTimeKind.Local).AddTicks(6285), "Gerilim", 600m, 60, new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified) },
+                    { 7, 3, new DateTime(2022, 9, 2, 16, 21, 5, 778, DateTimeKind.Local).AddTicks(6286), "Çizgili defter", 700m, 70, new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified) },
+                    { 8, 3, new DateTime(2022, 9, 2, 16, 21, 5, 778, DateTimeKind.Local).AddTicks(6287), "Kareli defter", 800m, 80, new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified) },
+                    { 9, 3, new DateTime(2022, 9, 2, 16, 21, 5, 778, DateTimeKind.Local).AddTicks(6288), "Çizgisiz defter", 900m, 90, new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified) }
+                });
+
+            migrationBuilder.InsertData(
+                table: "ProductFeatures",
+                columns: new[] { "Id", "Color", "Height", "ProductId", "Width" },
+                values: new object[,]
+                {
+                    { 1, "Kırmızı", 10, 1, 10 },
+                    { 2, "Mavi", 10, 2, 10 },
+                    { 3, "Sarı", 10, 3, 10 },
+                    { 4, "Kırmızı", 30, 4, 20 },
+                    { 5, "Mavi", 30, 5, 20 },
+                    { 6, "Sarı", 30, 6, 20 },
+                    { 7, "Kırmızı", 50, 7, 30 },
+                    { 8, "Mavi", 50, 8, 30 },
+                    { 9, "Sarı", 50, 9, 30 }
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ProductFeatures_ProductId",
+                table: "ProductFeatures",
+                column: "ProductId",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Products_CategoryId",
+                table: "Products",
+                column: "CategoryId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "ProductFeatures");
+
+            migrationBuilder.DropTable(
+                name: "Products");
+
+            migrationBuilder.DropTable(
+                name: "Categories");
+        }
+    }
+}

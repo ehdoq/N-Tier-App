f3b85d687e434b9fdee5afc9f336c887f8fa3310
AloKzMstf <alokazamustafa@gmail.com>
2022-09-05 11:49:59 +0300

    MVC(MVC) => Controllers(Dir) => ProductController eklendi. Index dosyası oluşturuldu. _ViewImports dosyasına ekleme yapıldı. _Layout dosyasına ekleme yapıldı.

---
+++ NLayerApp.MVC/Controllers/ProductsController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using NLayerApp.Core.Services;
+
+namespace NLayerApp.MVC.Controllers
+{
+    public class ProductsController : Controller
+    {
+        private readonly IProductService _productService;
+
+        public ProductsController(IProductService productService)
+        {
+            _productService = productService;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            return View(await _productService.GetProductsWithCategoryAsync());
+        }
+    }
+}
